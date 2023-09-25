76c50fa91de02a6a94acaa26ddae25a62ccb4db5
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-09-24 21:58:19 -0400

    copy/pasted from index.js

--- public/admin.js
+++ public/admin.js
@@ -4,70 +4,37 @@
 // place a submit button next to each text input
 // when the submit button is clickd retrieve the quantity from the associated text input and save the updated quantity to the server
 
-let apiURL = "http://localhost:3001/listBooks"
-let apiUpdate = "http://localhost:3001/updateBook"
-let apiDel = "http://localhost:3001/removeBook/{bookId}"
-let apiAddBook = "http://localhost:3001/addBook"
+let apiURL = "http://localhost:3001"
+// let apiUpdate = "http://localhost:3001/updateBook"
+// let apiDel = "http://localhost:3001/removeBook/{bookId}"
+// let apiAddBook = "http://localhost:3001/addBook"
 
 
 async function bookList (){
-    let response = await fetch(apiURL)
+    let response = await fetch(apiURL + "/listbooks")
     console.log("response from api", response)
 
-    let book = await response.json()
-    console.log(book)
+    let books = await response.json()
+    console.log(books)
 
-    book.forEach(displayBooks)
+    books.forEach(displayBooks)
 }
-
-// async function bookTitles (title){
-//     try{
-//     let response = await fetch (apiURL+ "/"+ "title" )
-//     console.log(response)
-//     } catch (error) {
-//         console.error("Could not find book title", title)
-//     }
-// }
-
-function displayBooks(book){
-    // creating a link to the html ID to be a connection from JS to Html
-    let root = document.querySelector("#root")
-
-    // creating a list by booktitles
-    let li = document.createElement('li')
-    li.textContent = book.title
-
-    // allowing admin to give value in a input bracket to change quantity's on hand 
-    let quantityInput = document.createElement("input")
-    quantityInput.value = book.quantity
-
-    // makes a save button
-    let saveButton = document.createElement("button")
-    saveButton.textContent = "Save"
-
-// everytime the save button is clicked it does a fetch/patch
-    saveButton.addEventListener("click",() =>{
-        fetch(apiUpdate, {
-            method: "PATCH",
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                id: books.id,
-                quantity: quantityInput.value
-            })
-        })
-    })
-// putting values from books.quantity and submit button into the list element
-    li.append(quantityInput,saveButton)
-    // putting the list into the rootID connected to the HTML page to display
-    root.append.li
-
+function displayBooks(book) {
+    let bookContainer = document.querySelector('.book-container')
+    bookContainer.innerHTML += `
+        <div class="col-sm-3">
+            <div class="card" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                    <input type="text" value="${book.quantity}"/>
+                </div>
+            </div>
+        </div>
+    `
 }
 
 bookList()
 displayBooks()
-
 
 // ---ACCEPTENACE CRITERIA---
 // When viewing the page in the browser, http://localhost:3001/listBooks must show the list of books specified in db.json.
